279b8b70fcb2a4670c56abc6d7c52b5a73ca0a7f
calleja <lecn99@hotmail.com>
2018-04-08 14:57:45 -0400

    initial commit

---
+++ controller2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Controller class to the equity trading platform
+"""
+
+import sys
+#sys.path.append('/home/lechuza/Documents/CUNY/data_607/assignment1/ass1_fromWork')
+#sys.path.append('/home/tio/Documents/CUNY/advancedProgramming/ass1_fromWork')
+sys.path.append('/usr/src/app/PROJECT_FOLDER')
+import engageUser as eu
+
+def main():
+    ''' building console interaction '''
+    engage=eu.Dialogue()
+    engage.engageUser()
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
